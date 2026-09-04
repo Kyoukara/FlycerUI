b15fd6e05a07d71b571e9d4e4539236b94427fa2
Kyo <89804765+Kyoukara@users.noreply.github.com>
2026-09-05 05:34:22 +0700

    none

--- build/header.lua
+++ build/header.lua
@@ -1,11 +1,30 @@
 --[[
-    
+
+     _      ___         ____  ______
+    | | /| / (_)__  ___/ / / / /  _/
+    | |/ |/ / / _ \/ _  / /_/ // /  
+    |__/|__/_/_//_/\_,_/\____/___/
+
+
     v{{VERSION}}  |  {{BUILD_DATE}}  |  {{DESCRIPTION}}
     
-    To view the source code, see the `src/` folder on the official GitHub repository.
+    ══════════════════════════════════════════════════════════════
+    FlycerUI is an independent modification of WindUI, developed to
+    support Commercial Use, Modification, Distribution, and Private Use.
+
+    This project is intended to provide a modified and independently
+    maintained version of the library, while respecting the original
+    project and its creator.
+
+    FlycerUI is not intended to compete with, replace, or claim ownership
+    of the original WindUI project. All original credits and references
+    remain attributed to their respective creator.
+
+    Visit the Official Github WindUI : https://github.com/Footagesus/WindUI
+    ══════════════════════════════════════════════════════════════
     
-    Author: Footagesus (Footages, .ftgs, oftgs)
-    Github: {{REPOSITORY}}
-    Discord: {{DISCORD}}
-    License: {{LICENSE}}
+    Author : Footagesus (Footages, .ftgs, oftgs)
+    Github : {{REPOSITORY}}
+    Discord : {{DISCORD}}
+    License : {{LICENSE}}
 ]]
