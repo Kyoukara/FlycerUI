3351057d6561cf9f4b9d8a42f5c95456c53b871d
Kyoukara <89804765+Kyoukara@users.noreply.github.com>
2026-09-04 22:34:43 +0000

    build: update dist/main.lua

--- dist/main.lua
+++ dist/main.lua
@@ -1,13 +1,32 @@
 --[[
-    
+
+     _      ___         ____  ______
+    | | /| / (_)__  ___/ / / / /  _/
+    | |/ |/ / / _ \/ _  / /_/ // /  
+    |__/|__/_/_//_/\_,_/\____/___/
+
+
     v1.6.66  |  2026-09-04  |  A Roblox UI Library based on WindUI.
     
-    To view the source code, see the `src/` folder on the official GitHub repository.
+    ══════════════════════════════════════════════════════════════
+    FlycerUI is an independent modification of WindUI, developed to
+    support Commercial Use, Modification, Distribution, and Private Use.
+
+    This project is intended to provide a modified and independently
+    maintained version of the library, while respecting the original
+    project and its creator.
+
+    FlycerUI is not intended to compete with, replace, or claim ownership
+    of the original WindUI project. All original credits and references
+    remain attributed to their respective creator.
+
+    Visit the Official Github WindUI : https://github.com/Footagesus/WindUI
+    ══════════════════════════════════════════════════════════════
     
-    Author: Footagesus (Footages, .ftgs, oftgs)
-    Github: https://github.com/Kyoukara/FlycerUI
-    Discord: 
-    License: MIT
+    Author : Footagesus (Footages, .ftgs, oftgs)
+    Github : https://github.com/Kyoukara/FlycerUI
+    Discord : 
+    License : MIT
 ]]
 
 type ConfigType__DARKLUA_TYPE_a={
